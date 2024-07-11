0d0f673e55226fb42530c0f61a540dc64843c8ed
Yoav K <yoav@arch>
2024-07-11 18:09:33 +0300

    fixed problem3

---
+++ problem3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+// Function prototypes
+bool is_prime(long unsigned n);
+
+int main(void)
+{
+    // long num = 13195;
+    long num = 600851475143;
+    printf("num = %li\n", num);
+    while (!is_prime(num))
+    {
+        for (int i=1; i<sqrtl(num); i++)
+            if (num % i == 0) 
+                num /= i;
+    }
+    printf("biggest prime factor: %i\n", num);
+}
+
+// O(sqrt n)
+bool is_prime(long unsigned n)
+{
+    for (long i=sqrt(n); i>1; i--) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
